db5974365fd2fdd60605ee7129e4d2028f2fb4f8
Andre Castro <andrefs.c@yahoo.com>
2022-08-01 15:47:29 +0100

    changing distance of scroll animation

--- js/animation-scroll.js
+++ js/animation-scroll.js
@@ -4,26 +4,26 @@ window.sr = ScrollReveal({ reset: true});
 
 
 sr.reveal('.about-wrapper', {
-    distance: '400px',
+    distance: '300px',
     duration: 3000 }, 
      );
 
 
 sr.reveal('.skills-wrapper', {
-    distance: '400px',
+    distance: '300px',
     origin: 'top',
     duration: 3000 }, 
     );
 
 sr.reveal('.projects-wrapper', {
-    distance: '400px',
+    distance: '300px',
     origin: 'bottom',
     duration: 3000 }, 
     );
 
 
 sr.reveal('.form-wrapper', {
-    distance: '400px',
+    distance: '300px',
     origin: 'top',
     duration: 3000 }, 
     );
